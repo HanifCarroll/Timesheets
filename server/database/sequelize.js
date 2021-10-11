fda2e10e7cb910c619c99f4f3269eca49778ce22
Hanif Carroll <HanifCarroll@gmail.com>
2021-10-11 09:57:12 -0400

    Add validation to timesheet model attributes.

--- server/database/sequelize.js
+++ server/database/sequelize.js
@@ -14,53 +14,87 @@ sequelize.define('Timesheet', {
   date: {
     type: DataTypes.DATEONLY,
     allowNull: false,
+    validate: {
+      isDate: true,
+      notEmpty: true,
+    },
   },
   client: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
   },
   project: {
     type: DataTypes.STRING,
     allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
   },
   projectCode: {
     type: DataTypes.STRING,
     allowNull: false,
     field: 'project_code',
+    validate: {
+      notEmpty: true,
+    },
   },
   hours: {
     type: DataTypes.FLOAT,
     allowNull: false,
+    validate: {
+      isNumeric: true,
+    },
   },
   isBillable: {
     type: DataTypes.BOOLEAN,
     allowNull: false,
     field: 'is_billable',
+    validate: {
+      isBoolean: true,
+    },
   },
   firstName: {
     type: DataTypes.STRING,
     allowNull: false,
     field: 'first_name',
+    validate: {
+      notEmpty: true,
+    },
   },
   lastName: {
     type: DataTypes.STRING,
     allowNull: false,
     field: 'last_name',
+    validate: {
+      notEmpty: true,
+    },
   },
   billableRate: {
     type: DataTypes.INTEGER,
     allowNull: false,
     field: 'billable_rate',
+    validate: {
+      isNumeric: true,
+    },
   },
   createdAt: {
     type: DataTypes.DATE,
     allowNull: false,
     field: 'created_at',
+    validate: {
+      isDate: true,
+    },
   },
   updatedAt: {
     type: DataTypes.DATE,
     allowNull: true,
     field: 'updated_at',
+    validate: {
+      isDate: true,
+    },
   }
 }, {
   tableName: 'timesheet',
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const asyncify = require('express-asyncify');
 const sequelize = require('./database/sequelize');
+const { ValidationError } = require("sequelize");
 
 
 const app = asyncify(express());
@@ -8,22 +9,35 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
-
 app.get('/api/timesheets', async (req, res) => {
-  const timesheets = await sequelize.models.Timesheet.findAll();
-  res.status(200).json(timesheets);
+  try {
+    const timesheets = await sequelize.models.Timesheet.findAll();
+
+    res.status(200).json(timesheets);
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong.' });
+  }
 });
 
 app.post('/api/timesheets', async (req, res) => {
-  const timesheetDto = { ...req.body };
-  timesheetDto.date = new Date(timesheetDto.date)
-  timesheetDto.createdAt = Date.now();
-  timesheetDto.updatedAt = null;
-  const timesheet = await sequelize.models.Timesheet.create(timesheetDto);
-  res.status(201).json(timesheet);
+  try {
+    const timesheetDto = {
+      ...req.body,
+      date: new Date(req.body.date),
+      createdAt: Date.now(),
+      updatedAt: null,
+    };
+    const timesheet = await sequelize.models.Timesheet.create(timesheetDto);
+
+    res.status(201).json(timesheet);
+  } catch (error) {
+    if (error instanceof ValidationError) {
+      res.status(400).json({ error: `${error.errors[0].path} is invalid.` });
+    } else {
+      res.status(500).json({ error: 'Something went wrong.' });
+    }
+  }
+
 });
 
 app.listen(port, () => {
